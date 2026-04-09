723cdcc19a749897b79569a7eb1ff90145daf225
MAXPOL <maxrol32@gmail.com>
2026-04-09 10:36:40 +0300

    Update ejabberd.sh

--- ejabberd.sh
+++ ejabberd.sh
@@ -32,14 +32,14 @@ y
 EOF
 
 cd /
-wget --no-check-certificate https://www.process-one.net/downloads/downloads-action.php?file=/19.05/ejabberd-19.05-0.x86_64.rpm
-mv downloads-action.php?file=%2F19.05%2Fejabberd-19.05-0.x86_64.rpm ejabberd-19.05-0.x86_64.rpm
-rpm -ivh ejabberd-19.05-0.x86_64.rpm
+wget https://github.com/processone/ejabberd/releases/download/26.03/ejabberd-26.03-1-linux-x64.run
+chmod +x ejabberd-26.03-1-linux-x64.run
+./ejabberd-26.03-1-linux-x64.run
 
 mysql -u root -p$passworddb -e "CREATE DATABASE ejabberd;"
 mysql -u root -p$passworddb -e "GRANT ALL ON ejabberd.* TO 'ejabberd'@'localhost' IDENTIFIED BY '$jabberdpassworddb';"
 mysql -u root -p$passworddb -e "FLUSH PRIVILEGES;"
-mysqldump -u root -p$passworddb ejabberd < /opt/ejabberd-19.05/lib/ejabberd-19.05/priv/sql/mysql.sql
+mysqldump -u root -p$passworddb ejabberd < /opt/ejabberd-26.03/lib/ejabberd-26.03/priv/sql/mysql.sql
 
 cat >> /opt/ejabberd/conf/ejabberd.yml <<EOF
 sql_type: mysql
@@ -49,7 +49,7 @@ sql_username: "ejabberd"
 sql_password: "$jabberdpassworddb"
 EOF
 
-cp /opt/ejabberd-19.05/bin/ejabberd.service /usr/lib/systemd/system/ejabberd.service
+cp /opt/ejabberd-26.03/bin/ejabberd.service /usr/lib/systemd/system/ejabberd.service
 
 systemctl enable ejabberd.service
 
@@ -57,13 +57,15 @@ sed -i 's/::/'$ipaddr'/g' /opt/ejabberd/conf/ejabberd.yml
 
 systemctl start ejabberd.service
 
-firewall-cmd --permanent --new-zone=vpn
-firewall-cmd --zone=vpn --change-interface=tun0
-firewall-cmd --permanent --zone=vpn --add-port={5280,5222}/tcp
+#firewall-cmd --permanent --new-zone=vpn
+#firewall-cmd --zone=vpn --change-interface=tun0
+#firewall-cmd --permanent --zone=vpn --add-port={5280,5222}/tcp
+firewall-cmd --permanent --zone=public --add-port={5280,5222}/tcp
 firewall-cmd --reload
 
-export PATH=$PATH:/opt/ejabberd-19.05/bin/
-ejabberdctl register admin ejabberdexample.com $adminjabberdpasswor
+export PATH=$PATH:/opt/ejabberd-26.03/bin/
+#ejabberdctl register admin ejabberdexample.com $adminjabberdpasswor
+ejabberdctl register admin localhost $adminjabberdpasswor
 
 echo "Enter in broser http:// $ipaddr :5280/admin"
 echo "You login: admin You password $adminjabberdpasswor"
